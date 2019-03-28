ca6abd6c6078530e1ebe769b68c37023507510ef
alirezaarv <alireza.arv.mail@gmail.com>
2019-03-28 11:12:17 +0430

    small change in app.js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,13 @@
-var stateIsLogin = true;
+$("#loginCard").hide();
 
-$("#changeLoginStateBtn").click(function() {
-  if (stateIsLogin) {
-    $("#firstText").text("عضو سایت نیستی؟");
-    $("#secondText").text("با کلیک بر روی فلش زیر میتونی رایگان ثبت نام کنی!");
-    $("#changeLoginStateBtn span").removeClass("fa-chevron-left");
-    $("#changeLoginStateBtn span").addClass("fa-chevron-right");
-    stateIsLogin = false;
-  } else {
-    $("#firstText").text("قبلا عضو شدی؟");
-    $("#secondText").text("برای ورود به سایت بر روی فلش زیر کلیک کن!");
-    $("#changeLoginStateBtn span").removeClass("fa-chevron-right");
-    $("#changeLoginStateBtn span").addClass("fa-chevron-left");
-    stateIsLogin = true;
-  }
+$("#changeToLoginBtn").click(function() {
+  $("#singupCard").hide();
+  $("#loginCard").show();
+});
+
+$("#changeToSignupBtn").click(function() {
+  $("#loginCard").hide();
+  $("#singupCard").show();
 });
 
 $("#navBtn1").click(function() {
